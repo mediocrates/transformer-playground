add dropout arg to transformer, fix ordering of multiplication, timing of taking mean, and extra factor of `sqrt(d_model)` that I was previously misunderstanding

transformer.py
@@ -7,12 +7,12 @@ from util import FeedForward, MultiHeadDecoderAttention, PositionalEncoding, Sub
 
 class Decoder(nn.Module):
 
-    def __init__(self, d_model, d_k, d_v, h, d_ff=None):
+    def __init__(self, d_model, d_k, d_v, h, d_ff=None, p_dropout=0.1):
         if d_v is None:
             d_v = d_k
         super().__init__()
-        self.self_attention = Sublayer(MultiHeadDecoderAttention(d_model, d_k, d_v, h))
-        self.ffn = Sublayer(FeedForward(d_model, d_ff))
+        self.self_attention = Sublayer(MultiHeadDecoderAttention(d_model, d_k, d_v, h), p_dropout=p_dropout)
+        self.ffn = Sublayer(FeedForward(d_model, d_ff), p_dropout=p_dropout)
 
     def forward(self, x):
         return self.ffn(self.self_attention(x))
@@ -20,9 +20,9 @@ class Decoder(nn.Module):
 
 class DecoderStack(nn.Module):
 
-    def __init__(self, d_model, d_k, d_v, h, d_ff, n):
+    def __init__(self, d_model, d_k, d_v, h, d_ff, n, p_dropout=0.1):
         super().__init__()
-        self.decoders = nn.Sequential(*[Decoder(d_model, d_k, d_v, h, d_ff) for _ in range(n)])
+        self.decoders = nn.Sequential(*[Decoder(d_model, d_k, d_v, h, d_ff, p_dropout=p_dropout) for _ in range(n)])
 
     def forward(self, x):
         for decoder in self.decoders:
@@ -32,11 +32,11 @@ class DecoderStack(nn.Module):
 
 class DecoderOnlyTransformer(nn.Module):
 
-    def __init__(self, d_vocab, d_model, d_k, d_v=None, h=8, d_ff=None, n=6):
+    def __init__(self, d_vocab, d_model, d_k, d_v=None, h=8, d_ff=None, n=6, p_dropout=0.1):
         super().__init__()
         self.embedding = nn.Embedding(num_embeddings=d_vocab, embedding_dim=d_model)
         self.pe = PositionalEncoding(d_model)
-        self.decoders = DecoderStack(d_model, d_k, d_v, h, d_ff, n)
+        self.decoders = DecoderStack(d_model, d_k, d_v, h, d_ff, n, p_dropout=p_dropout)
 
     def forward(self, tgt, src, delim_token=1):
         """
@@ -48,12 +48,12 @@ class DecoderOnlyTransformer(nn.Module):
         bs = tgt.shape[0]
         d_model = tgt.shape[-1]
         combined = torch.cat([tgt, torch.tensor([delim_token]).to(tgt.device).expand(bs, -1), src], dim=-1)
-        a = self.embedding(combined) * np.sqrt(d_model)
+        a = self.embedding(combined)
         a += self.pe.forward(a)
         return torch.softmax(
-            self.embedding.weight.transpose(-2, -1) @ self.decoders(a),
+            self.decoders(a).mean(dim=-2) @ self.embedding.weight.transpose(-2, -1) / np.sqrt(3 * d_model),
             dim=-1
-        ).mean(dim=-2)
+        )
 
     def predict(self, src=None, tgt=None, start_token=0, delim_token=1, end_token=2, max_len=50):
         device = next(self.parameters()).device