129cd8e8e045c43a3b7a5e624816369ae744c39c
mediocrates <justin.shih@gmail.com>
2023-01-12 17:52:01 -0600

    finalize weight tying (with change of default to `True`), and docstring for text generation method `predict`.

--- transformer.py
+++ transformer.py
@@ -28,29 +28,41 @@ class Decoder(nn.Module):
 
 class DecoderOnlyTransformer(nn.Module):
 
-    def __init__(self, d_vocab, d_model, d_k, d_v=None, h=8, d_ff=None, n=12, p_dropout=0.1, tie_weights=False):
+    def __init__(self, d_vocab, d_model, d_k, d_v=None, h=8, d_ff=None, n=12, p_dropout=0.1, tie_weights=True):
         super().__init__()
         self.d_model = d_model
         self.embedding = nn.Embedding(num_embeddings=d_vocab, embedding_dim=d_model)
-        # torch.nn.init.xavier_uniform_(self.embedding.weight)
         self.pe = PositionalEncoding(d_model, p_dropout=p_dropout)
         self.decoders = nn.Sequential(*[Decoder(d_model, d_k, d_v, h, d_ff, p_dropout=p_dropout) for _ in range(n)])
         self.final = nn.Linear(d_model, d_vocab)
 
-        # TODO (justin): this doesn't exactly work yet
         self.tie_weights = tie_weights
         if tie_weights:
-            self.embedding.weight = self.final.weight  # weight-tying
+            # this works because nn.Embedding(x, y).weight is x by y, and nn.Linear(y, x).weight is ALSO x by y
+            # and I think that by setting embedding.weight to something else, the old reference to nn.weight disappears.
+            # but not 100% sure it goes away due to having 0 references
+            self.embedding.weight = self.final.weight
 
     def forward(self, combined):
         a = self.embedding(combined)
         if self.tie_weights:
+            # nn.Embedding weights are initialized to N(0, 1)
+            # nn.Linear    weights are initialized to U(-sqrt(k), sqrt(k)) where k = 1/d_model
+            # X = U(-x, x) has std(X) =  x/sqrt(3), so try to preserve the variance in the embedding layer by multiplying by sqrt(3)/x = sqrt(3 * d_model)
             a *= np.sqrt(3 * self.d_model)
         a += self.pe.forward(a)
-        # self.decoders(a) @ self.embedding.weight.transpose(-2, -1) / np.sqrt(3 * d_model),
         return self.final(self.decoders(a))
 
-    def predict(self, prompt=None, start_token=0, end_token=2, max_len=50):
+    def predict(self, prompt=None, start_token=0, end_token=2, context_len=128, max_len=50):
+        """
+
+        Args:
+            prompt: Tokenized tensor of indices corresponding to words.
+            start_token: Start token.
+            end_token: End token.
+            context_len: Max length of context when feeding into model.
+            max_len: Max length of sequence before terminating generation.
+        """
         device = next(self.parameters()).device
         if prompt is not None:
             combined = prompt
@@ -59,7 +71,7 @@ class DecoderOnlyTransformer(nn.Module):
         pick = None
         while pick != end_token and combined.shape[-1] < max_len:
             self.eval()
-            out = self(combined.unsqueeze(0))
+            out = self(combined[-context_len:].unsqueeze(0))
             self.train()
             pick = out[0, -1].argmax(dim=-1).unsqueeze(0)
             combined = torch.cat([combined, pick], dim=-1)
